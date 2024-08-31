bba2dec6fba2114dca51562dedbef62112bf394c
Mr Alireza Noroozi <62061702+noroozi412@users.noreply.github.com>
2024-08-31 21:50:48 +0330

    Delete session4/Hiabernate directory

--- session4/Hiabernate/src/main/java/com/hiabernate/hiabernate/Repository‌/CorruptedItemRepository.java
+++
@@ -1,7 +0,0 @@
-package com.hiabernate.hiabernate.Repository‌;
-import com.hiabernate.hiabernate.Model.modelCorruptedItem;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface CorruptedItemRepository extends JpaRepository<modelCorruptedItem,Long> {
-}
--- session4/Hiabernate/src/main/java/com/hiabernate/hiabernate/Repository‌/ItemRepository.java
+++
@@ -1,7 +0,0 @@
-package com.hiabernate.hiabernate.Repository‌;
-
-import com.hiabernate.hiabernate.Model.modelItem;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface ItemRepository extends JpaRepository<modelItem,Long> {
-}
